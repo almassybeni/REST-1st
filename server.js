5031191280f1765dd5aff491af7d13435a8a3654
almassybeni <almassy.benedek.solt@gmail.com>
2025-09-19 09:37:27 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ const courses = [
     {id: 3,name: 'course3',}
 ]
 
+//GET végpont egy szöveges üzener visszaküldésére
 app.get('/hello', (req, res ) => {
     res.send(' sziaaaaaa');
 })
 
-app.listen(port,() => console.log(`A webszerver figyel a ${port} számú porton`))
 
 app.get('/api/courses', (req, res) => {
     res.json(courses);
 })
+//GET végpont egy kiválasztott kurzus lekérésére
+app.get('/api/courses/:id' , (req, res) => {
+    //Kurzus adatok keresése a courses tömbben
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    res.json(course);
+})
+
+app.listen(port,() => console.log(`A webszerver figyel a ${port} számú porton`))
